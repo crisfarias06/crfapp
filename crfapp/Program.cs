68736f4fb2ff8070fe6115b11a65f9b81530ea7f
crfapp <cristiana.farias@etec.sp.gov.br>
2022-10-27 22:06:41 -0300

    Projeto inical

---
+++ crfapp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crfapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int numero2;
+            int total;
+
+            numero = 11;
+            numero2 = 42;
+
+            total = numero + numero2;
+
+
+            Console.WriteLine("tecnico informatica");
+            Console.WriteLine("boa noite");
+            Console.WriteLine("27 /10/2022");
+            Console.WriteLine("total");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
